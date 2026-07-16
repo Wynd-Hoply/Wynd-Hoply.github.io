0be19fc2f744132238ab00a152a0943444bbccf8
Wynd-Hoply <wyndhoply@gmail.com>
2026-07-16 10:07:33 -0300

    Passagem de arquivos prontos

---
+++ script.js
@@ -0,0 +1,49 @@
+// Cria uma constante que puxa a id "Header"
+const header = document.getElementById("header");
+// Quando descer um pouco o Header vai fazer algo
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 90) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+// Cria uma constante que puxa a id "Body"
+const body = document.getElementById("body");
+// Quando descer um pouco o Body vai fazer algo
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 90) {
+    body.classList.add("scrolled");
+  } else {
+    body.classList.remove("scrolled");
+  }
+});
+
+
+window.addEventListener("scroll", () => {
+
+    if (window.innerWidth <= 900) {
+        header.classList.remove("scrolled");
+        return;
+    }
+
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+
+});
+
+document.querySelectorAll('.nav-menu a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const destino = document.querySelector(this.getAttribute('href'));
+
+    destino.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
